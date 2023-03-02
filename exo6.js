094bbf1a0286ef89ce9b10651cf74813e12ba579
59013-97-11 <59013-97-11@exchange.ad.afpanet>
2023-03-02 10:26:47 +0100

    ege

--- exo6.js
+++ exo6.js
@@ -10,22 +10,18 @@ function justePrix(){
     limiteTentative++
 
     if(prix < nombreAleatoire){
-        console.log("Choisissez un nombre plus élevé" + limiteTentative)
         document.getElementById("résultat").innerHTML = "choisissez un nombre plus élevé, vous êtes à votre "  + limiteTentative +" tentatives !"
     }
 
     else if(prix > nombreAleatoire){
-        console.log("Choisissez un nombre moins élevé" + limiteTentative)
         document.getElementById("résultat").innerHTML = "choisissez un nombre moins élevé, vous êtes à votre "  + limiteTentative +" tentatives !"
     }
         
     else if(prix == nombreAleatoire){
-        console.log("Félicitation vous avez trouvé le nombre exact" + limiteTentative)
-        document.getElementById("résultat").innerHTML = "félicitation vous avez trouver le nombre exact à votre "  + limiteTentative +" tentatives !"
+        document.getElementById("résultat").innerHTML = "félicitation vous avez trouvé le nombre exact à votre première tentative !"
     }
         
     if(limiteTentative > 10){
-        console.log("Malheureusement, vous avez eu 10 tentatives et vous avez échoué")
         document.getElementById("résultat").innerHTML = "malheuresement, vous avez eu 10 tentatives et vous avez échoué."
     }
 }
